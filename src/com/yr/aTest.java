b7d4a200073ae6564bbf196b7d4bca1327d4fcbe
STRONGgitgit <42628589+STRONGgitgit@users.noreply.github.com>
2018-08-23 21:49:50 +0800

    Update aTest.java
    删除了方法

--- src/com/yr/aTest.java
+++ src/com/yr/aTest.java
@@ -2,10 +2,6 @@ package com.yr;
 
 public class aTest {
 	public static void main(String[] args) {
-		System.out.println("-----1---1");
-		System.out.println("--------1");
-		System.out.println("--------2");
-		System.out.println("-----2---2");
 		aTest  a = new aTest();
 		a.add();
 	}
