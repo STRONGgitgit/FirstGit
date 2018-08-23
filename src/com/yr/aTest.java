293165309d7cd0c9333e328c4200bb7871276513
STRONGgitgit <1024474540@qq.com>
2018-08-23 22:07:33 +0800

    代码格式化

--- src/com/yr/aTest.java
+++ src/com/yr/aTest.java
@@ -2,15 +2,11 @@ package com.yr;
 
 public class aTest {
 	public static void main(String[] args) {
-		aTest  a = new aTest();
+		aTest a = new aTest();
 		a.add();
 	}
-	public void add(){
-		int i =0;
-	
-		
-		
-		
+
+	public void add() {
+		int i = 0;
 	}
-	
 }
