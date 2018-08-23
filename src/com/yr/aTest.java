3b0f8c6da6b54a0833cf214f70b8522a06982668
STRONGgitgit <1024474540@qq.com>
2018-08-23 21:33:11 +0800

    合并

--- src/com/yr/aTest.java
+++ src/com/yr/aTest.java
@@ -2,7 +2,8 @@ package com.yr;
 
 public class aTest {
 	public static void main(String[] args) {
-		System.out.println("8888888");
+		System.out.println("=======");
+		System.out.println("--------");
 	}
 
 	public void add(){
