f9fb99988a3cb558d3438bf1afa635918c697f3f
STRONGgitgit <1024474540@qq.com>
2018-08-23 21:48:48 +0800

    调用了方法

--- src/com/yr/aTest.java
+++ src/com/yr/aTest.java
@@ -6,6 +6,8 @@ public class aTest {
 		System.out.println("--------1");
 		System.out.println("--------2");
 		System.out.println("-----2---2");
+		aTest  a = new aTest();
+		a.add();
 	}
 	public void add(){
 		System.out.println("hahahah");
